7cdc88ccfe614ba8c72aa8b5e0b1821b8fbef846
nsteinme <nick.steinmetz@gmail.com>
2016-07-31 13:10:56 +0100

    add schmitt trigger

---
+++ analysis/helpers/schmittTimes.m
@@ -0,0 +1,13 @@
+
+
+function [flipTimes, flipsUp, flipsDown] = schmittTimes(t, sig, thresh)
+% function [flipTimes, flipsUp, flipsDown] = schmittTimes(t, sig, thresh)
+%
+% thresh is [low high]
+
+schmittSig = schmitt(sig, thresh);
+
+flipsDown = t(schmittSig(1:end-1)==1 & schmittSig(2:end)==-1);
+flipsUp = t(schmittSig(1:end-1)==-1 & schmittSig(2:end)==1);
+
+flipTimes = sort([flipsUp flipsDown]);
