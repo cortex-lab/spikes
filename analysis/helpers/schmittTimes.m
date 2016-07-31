addc7d480e8bafa7dd9e2ecaa7fc55ee13560613
nsteinme <nick.steinmetz@gmail.com>
2016-07-31 13:17:57 +0100

    bug fix schmitt times

--- analysis/helpers/schmittTimes.m
+++ analysis/helpers/schmittTimes.m
@@ -5,9 +5,12 @@ function [flipTimes, flipsUp, flipsDown] = schmittTimes(t, sig, thresh)
 %
 % thresh is [low high]
 
+t = t(:); % make column
+sig = sig(:);
+
 schmittSig = schmitt(sig, thresh);
 
 flipsDown = t(schmittSig(1:end-1)==1 & schmittSig(2:end)==-1);
 flipsUp = t(schmittSig(1:end-1)==-1 & schmittSig(2:end)==1);
 
-flipTimes = sort([flipsUp flipsDown]);
+flipTimes = sort([flipsUp; flipsDown]);
