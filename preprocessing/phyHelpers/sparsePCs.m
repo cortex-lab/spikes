eb53e09456283401b3953d160a6c724f3f55fbf6
nsteinme <nick.steinmetz@gmail.com>
2016-09-01 17:27:13 +0100

    update to sparsePCs

--- preprocessing/phyHelpers/sparsePCs.m
+++ preprocessing/phyHelpers/sparsePCs.m
@@ -13,18 +13,22 @@ nChannels = double(max(pcFeatInd(:))+1);
 
 
 rowInds = repmat([1:nSpikes]', [nPCchans*nFeat,1]);
-colIndsTemp = zeros(size(rowInds));
+colIndsTemp = zeros(nSpikes*nPCchans,1);
 
 for q = 1:nPCchans
     colIndsTemp((q-1)*nSpikes+1:q*nSpikes) = pcFeatInd(spikeTemplates+1,q)+1;
 end
 
-pcFeatRS = zeros(nSpikes*nPCchans*nFeat,1);
 colInds = zeros(nSpikes*nPCchans*nFeat,1);
 for thisFeat = 1:3
-    pcFeatRS((thisFeat-1)*nSpikes*nPCchans:thisFeat*nSpikes*nPCchans) = ...
-        double(reshape(squeeze(pcFeat(:,thisFeat,:)), [nSpikes*nPCchans 1]));
-    colInds((thisFeat-1)*nSpikes*nPCchans:thisFeat*nSpikes*nPCchans) = ...
+    colInds(((thisFeat-1)*nSpikes*nPCchans+1):thisFeat*nSpikes*nPCchans) = ...
         (colIndsTemp-1)*nFeat+thisFeat;
 end
-S = sparse(rowInds, colInds, pcFeatRS, nSpikes, nChannels);
+clear colIndsTemp
+
+pcFeatRS = zeros(nSpikes*nPCchans*nFeat,1);
+for thisFeat = 1:3
+    pcFeatRS(((thisFeat-1)*nSpikes*nPCchans+1):thisFeat*nSpikes*nPCchans) = ...
+        double(reshape(squeeze(pcFeat(:,thisFeat,:)), [nSpikes*nPCchans 1]));
+end
+S = sparse(rowInds, colInds, pcFeatRS, nSpikes, nChannels*nFeat);
