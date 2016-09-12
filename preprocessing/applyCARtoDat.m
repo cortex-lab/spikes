adf904786ee9593290fbdd9047e2ef7d3d94f3fd
m-okun <mush@cs.huji.ac.il>
2016-09-12 16:16:53 +0100

    Add files via upload
    Added support for creating the outputs in a different directory.

--- preprocessing/applyCARtoDat.m
+++ preprocessing/applyCARtoDat.m
@@ -1,14 +1,15 @@
 
 
 
-function medianTrace = applyCARtoDat(filename, nChansTotal)
+function medianTrace = applyCARtoDat(filename, nChansTotal, outputDir)
 % Subtracts median of each channel, then subtracts median of each time
-% point. 
+% point.
 %
 % filename should include the extension
+% outputDir is optional, by default will write to the directory of the input file
 %
 % should make chunk size as big as possible so that the medians of the
-% channels differ little from chunk to chunk. 
+% channels differ little from chunk to chunk.
 chunkSize = 1000000;
 
 fid = []; fidOut = [];
@@ -17,52 +18,57 @@ d = dir(filename);
 nSampsTotal = d.bytes/nChansTotal/2;
 nChunksTotal = ceil(nSampsTotal/chunkSize);
 try
-
-fid = fopen(filename, 'r');
-
-ext = filename(end-2:end);
-fidOut = fopen([filename(1:end-4) '_CAR.' ext], 'w');
-
-
-% theseInds = 0; 
-chunkInd = 1;
-medianTrace = zeros(1, nSampsTotal);
-while 1
+  
+  [pathstr, name, ext] = fileparts(filename);
+  fid = fopen(filename, 'r');
+  if nargin < 3
+    outputFilename  = [pathstr filesep name '_CAR' ext];
+    mdTraceFilename = [pathstr filesep name '_medianTrace.mat'];
+  else
+    outputFilename  = [outputDir filesep name '_CAR' ext];
+    mdTraceFilename = [outputDir filesep name '_medianTrace.mat'];
+  end
+  fidOut = fopen(outputFilename, 'w');
+  
+  % theseInds = 0;
+  chunkInd = 1;
+  medianTrace = zeros(1, nSampsTotal);
+  while 1
     
     fprintf(1, 'chunk %d/%d\n', chunkInd, nChunksTotal);
     
     dat = fread(fid, [nChansTotal chunkSize], '*int16');
     
     if ~isempty(dat)
-    
-%         theseInds = theseInds(end):theseInds(end)+chunkSize-1;        
-            
-        dat = bsxfun(@minus, dat, median(dat,2)); % subtract median of each channel
-        tm = median(dat,1);
-        dat = bsxfun(@minus, dat, tm); % subtract median of each time point
-        fwrite(fidOut, dat, 'int16');
-        medianTrace((chunkInd-1)*chunkSize+1:(chunkInd-1)*chunkSize+numel(tm)) = tm;
-            
+      
+      %         theseInds = theseInds(end):theseInds(end)+chunkSize-1;
+      
+      dat = bsxfun(@minus, dat, median(dat,2)); % subtract median of each channel
+      tm = median(dat,1);
+      dat = bsxfun(@minus, dat, tm); % subtract median of each time point
+      fwrite(fidOut, dat, 'int16');
+      medianTrace((chunkInd-1)*chunkSize+1:(chunkInd-1)*chunkSize+numel(tm)) = tm;
+      
     else
-        break
+      break
     end
     
     chunkInd = chunkInd+1;
-end
-
-save([filename(1:end-4) '_medianTrace.mat'], 'medianTrace');
-
+  end
+  
+  save(mdTraceFilename, 'medianTrace');
+  
 catch me
-    
-    if ~isempty(fid)
-        fclose(fid);
-    end
-    
-    if ~isempty(fidOut)
-        fclose(fidOut);
-    end
-    
-    
-    rethrow(me)
-    
+  
+  if ~isempty(fid)
+    fclose(fid);
+  end
+  
+  if ~isempty(fidOut)
+    fclose(fidOut);
+  end
+  
+  
+  rethrow(me)
+  
 end
