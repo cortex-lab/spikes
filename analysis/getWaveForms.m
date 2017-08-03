8eefb7137e7f91f7ef9283439a59fb22c5c251c1
goatsofnaxos <ces2001@cumc.columbia.edu>
2017-08-03 10:40:46 -0400

    Edited documentation

--- analysis/getWaveForms.m
+++ analysis/getWaveForms.m
@@ -6,23 +6,24 @@ function wf = getWaveForms(gwfparams)
 %
 % Contributed by C. Schoonover and A. Fink
 %
-% % INPUT
-% gwfparams.dataDir = '/path/to/data/';                                         % KiloSort/Phy output folder
-% gwfparams.fileName = 'data.dat';                                              % .dat file containing the raw 
-% gwfparams.dataType = 'int16';                                                 % Data type of .dat file (this should be BP filtered)
-% gwfparams.nCh = 32;                                                           % Number of channels that were streamed to disk in .dat file
-% gwfparams.wfWin = [-40 41];                                                   % Number of samples before and after spiketime to include in waveform
-% gwfparams.nWf = 2000;                                                         % Number of waveforms per unit to pull out
-% [gwfparams.spikeClusters, gwfparams.spikeTimes] = phy2mat(gwfparams.dataDir); % Spike times of clusters classified as 'good' in Phy
-%   .spikeClusters and .spikeTimes are vectors of the same length, can be
-%   any set of clusters and associated spike times you wish to use
+% % EXAMPLE INPUT
+% gwfparams.dataDir = '/path/to/data/';    % KiloSort/Phy output folder
+% gwfparams.fileName = 'data.dat';         % .dat file containing the raw 
+% gwfparams.dataType = 'int16';            % Data type of .dat file (this should be BP filtered)
+% gwfparams.nCh = 32;                      % Number of channels that were streamed to disk in .dat file
+% gwfparams.wfWin = [-40 41];              % Number of samples before and after spiketime to include in waveform
+% gwfparams.nWf = 2000;                    % Number of waveforms per unit to pull out
+% gwfparams.spikeTimes =    [2,3,5,7,8,9]; % Vector of cluster spike times (in samples) same length as .spikeClusters
+% gwfparams.spikeClusters = [1,2,1,1,1,2]; % Vector of cluster IDs (Phy nomenclature)   same length as .spikeTimes
 %
 % % OUTPUT
-% wf.unitIDs        % Cluster IDs (Phy nomenclature)
-% wf.spikeTimeKeeps % Which spike times were used for the waveforms
-% wf.waveForms      % Individual waveforms
-% wf.waveFormsMean  % Average of all waveforms
-% 
+% wf.unitIDs                               % [nClu,1]            List of cluster IDs; defines order used in all wf.* variables
+% wf.spikeTimeKeeps                        % [nClu,nWf]          Which spike times were used for the waveforms
+% wf.waveForms                             % [nClu,nWf,nCh,nSWf] Individual waveforms
+% wf.waveFormsMean                         % [nClu,nCh,nSWf]     Average of all waveforms (per channel)
+%                                          % nClu: number of different clusters in .spikeClusters
+%                                          % nSWf: number of samples per waveform
+%
 % % USAGE
 % wf = getWaveForms(gwfparams);
 
