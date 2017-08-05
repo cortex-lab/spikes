694b6c495b123fcca978307e91853a23565bb1ee
nsteinme <nick.steinmetz@gmail.com>
2017-08-05 22:08:23 +0100

    first commit of popRasterViewer

---
+++ visualization/popRasterViewer.m
@@ -0,0 +1,394 @@
+
+function popRasterViewer(sp, eventData, traces, auxVid, anatData, pars)
+%
+% Viewer for ephys data collected during the task
+%
+% Displays:
+% - Rasters from multiple probes
+% -- Can make raster y-axis into cluster identity (sorted by depth) or probe depth
+% -- Can make raster colorization be different things, like depth or spike
+% amplitude or some feature
+%
+% - Behavioral traces (lick and wheel)
+%
+% - Movies (eye and face)
+% -- movies loop over the current window
+%
+% - Event times
+%
+% Inputs:
+% - sp: struct with
+%   - st, [nSp 1] spike times
+%   - clu, [nSp 1] cluster labels
+%   - cids, [nClusters 1] vector of the unique cluster numbers
+%   - yAxOrderings, [nOrderings 1] struct array with:
+%       - name, string
+%       - yPos, [nClusters 1] giving a y-axis position for each cluster
+%   - colorings, [nColorings 1] struct array with:
+%       - name, string
+%       - colors, [nClusters 3] colors
+% - auxVids: faceVid, eyeVid
+% - traces: struct array with
+%   - t, timestamps
+%   - v, values
+%   - name, string
+% - eventData: struct array with
+%   - color
+%   - name
+%   - times
+%
+% Controls:
+% - 'y': choose raster y-axis
+% - 'c': choose raster colorization
+% - left/right arrow: move forward/back in time
+% - up/down: make time window bigger/smaller
+% - click on raster: draw a vertical line
+% - click on event name: toggle it on/off
+% - click on trace name: select it - click again: turn off
+% - i/j/k/l: move selected trace up/down or scale it
+% - '-'/'=': make raster ticks shorter/taller
+% - 't': jump to time
+% - 'm': hide mua (NOT IMPL)
+% - 's'/'f': movies play slower or faster
+% - 'p': play/pause movies
+
+% todo: 
+% - add anatomical labels
+% - option to drop mua
+% - add LFP 
+% - add screen rendering
+
+for e = 1:length(eventData)
+    eventData(e).visible = true;
+end
+for t = 1:length(traces)
+    traces(t).visible = true;
+    traces(t).scale = 0.2/(max(traces(t).v)-min(traces(t).v)); % percentage of window
+    traces(t).offset = (t-1)/length(traces)+1/(2*length(traces));
+end
+ud.eventData = eventData;
+ud.traces = traces;
+ud.auxVid = auxVid;
+
+if isfield(pars, 'startTime'); startTime = pars.startTime; else; startTime = 10; end
+
+params.windowSize = 5; % for timeWin
+params.currT = startTime;
+params.rasterScale = 1; % height of ticks
+params.window = params.windowSize*0.5*[-1 1]+params.currT;
+params.binSize = 0.002;
+params.playVid = false;
+params.posLims = [0 numel(sp.cids)];
+params.yAxInd = 1; % which one is currently used
+params.colorInd = 1; 
+params.selectedTrace = 1;
+params.movieT = params.window(1);
+params.isPlaying = true;
+params.timerPeriod = 1/30;
+params.movieRate = 1; % 1 = realtime
+params.lastRealTime = now;
+
+% re-scale all the y-orderings to be the same so things don't re-scale when
+% the ordering covers a different range
+for y = 1:numel(sp.yAxOrderings)
+    yp = sp.yAxOrderings(y).yPos;
+    mn = min(yp); mx = max(yp);
+    sp.yAxOrderings(y).yLabels = [mn mx];
+    sp.yAxOrderings(y).yPos = (yp-mn)/mx*diff(params.posLims)+params.posLims(1);
+end
+ud.sp = sp;
+ud.params = params;
+
+f = figure;
+
+ud.f = f;
+hands = createPlots(ud);
+ud.hands = hands;
+updatePlots(ud);
+recolor(ud);
+
+set(f, 'UserData', ud);
+set(f, 'KeyPressFcn', @(f,k)keyboardPressCallback(f, k));
+
+myTimer = timer(...
+    'Period',params.timerPeriod,...
+    'ExecutionMode','fixedRate',...
+    'TimerFcn',@(h,eventdata)updateMovies(f));
+
+set(f, 'CloseRequestFcn', @(s,~)closeFigure(s, myTimer));
+
+start(myTimer);
+
+function h = createPlots(ud)
+sp = ud.sp;
+ed = ud.eventData;
+tr = ud.traces;
+p = ud.params;
+
+h = [];
+h.rasterAx = axes('Position', [0.03 0.03 0.7 0.96]);
+set(h.rasterAx, 'Color', 'k');
+set(h.rasterAx, 'ButtonDownFcn', @(~,k)rastClick(k, ud.f));
+hold(h.rasterAx, 'on');
+for c = 1:length(sp.cids)
+    thisH = plot(0,0,'w-');
+    set(thisH, 'HitTest', 'off');
+    h.rasterHands(c) = thisH;
+end
+box(h.rasterAx, 'off');
+
+for e = 1:length(ed)
+    thisH = plot(0,0,'-', 'Color', ed(e).color);
+    set(thisH, 'HitTest', 'off');
+    h.eventHands(e) = thisH;
+end
+
+for t = 1:length(tr)
+    thisH = plot(0,0,'-', 'Color', tr(t).color);
+    set(thisH, 'HitTest', 'off');
+    h.traceHands(t) = thisH;
+end
+
+h.userLine = plot([0 0], p.posLims, 'w');
+
+h.ColorDisplay = annotation('textbox', [0.75 0.9 0.1 0.1], 'String', '', 'EdgeColor', 'none');
+h.yPosDisplay = annotation('textbox', [0.75 0.85 0.1 0.1], 'String', '', 'EdgeColor', 'none');
+
+for e = 1:length(ed)
+    thisH = annotation('textbox', [0.75 0.8-length(tr)*0.05-e*0.05 0.1 0.1], 'String', ['[E] ' ed(e).name]);
+    thisH.BackgroundColor = 'k';
+    thisH.FontWeight = 'bold';
+    thisH.Color = ed(e).color;
+    set(thisH, 'ButtonDownFcn', @(~,~)toggleEvent(ud.f, e))
+    h.evLeg(e) = thisH;
+end
+
+for t = 1:length(tr)
+    thisH = annotation('textbox', [0.75 0.8-t*0.05 0.1 0.1], 'String', ['[T] ' tr(t).name]);
+    thisH.BackgroundColor = 'k';
+    thisH.Color = tr(t).color;
+    set(thisH, 'ButtonDownFcn', @(~,~)selectTrace(ud.f, t))
+    h.trLeg(t) = thisH;
+end
+
+if ~isempty(ud.auxVid)
+    for v = 1:numel(ud.auxVid)
+        ax = axes('Position', [0.75 0.05+(v-1)*0.25 0.23 0.25]);
+        ud.auxVid(v).f(ax, p.movieT, ud.auxVid(v).data);
+        h.auxAxes(v) = ax;
+    end
+    h.movieTimeLine = plot(h.rasterAx,[0 0], p.posLims, 'w:');
+end
+
+
+function updateMovies(f)
+ud = get(f, 'UserData');
+p = ud.params;
+if p.isPlaying
+    newT = p.movieT+(now-p.lastRealTime)*24*3600*p.movieRate;
+    if newT>p.window(2)
+        newT = p.window(1);
+    end
+    p.lastRealTime = now;
+    p.movieT = newT;
+    ud.params = p;
+    set(f, 'UserData', ud);
+    if ~isempty(ud.auxVid)
+        for v = 1:numel(ud.auxVid)
+            ax = ud.hands.auxAxes(v);
+            ud.auxVid(v).f(ax, p.movieT, ud.auxVid(v).data);
+        end
+    end
+    set(ud.hands.movieTimeLine, 'XData', p.movieT*[1 1]);
+    title(ud.hands.auxAxes(v), sprintf('rate = %.2f', p.movieRate));
+    drawnow;
+end
+
+function updatePlots(ud)
+
+sp = ud.sp;
+p = ud.params;
+h = ud.hands;
+ed = ud.eventData;
+tr = ud.traces;
+        
+incl = sp.st>p.window(1) & sp.st<p.window(2);
+st = sp.st(incl); clu = sp.clu(incl);
+for c = 1:length(sp.cids)
+    [rasterX,yy] = rasterize(st(clu==sp.cids(c)));
+    rasterY = yy*p.rasterScale+sp.yAxOrderings(p.yAxInd).yPos(c); % yy is of the form [0 1 NaN 0 1 NaN...] so just need to add trial number to everything
+    set(h.rasterHands(c), 'XData', rasterX, 'YData', rasterY);
+end
+
+for e = 1:length(ed)
+    if ed(e).visible
+        incl = ed(e).times>p.window(1) & ed(e).times<p.window(2);
+        if sum(incl)>0
+            [xx,yy] = rasterize(ed(e).times(incl));    
+            set(h.eventHands(e), 'XData', xx, 'YData', yy*p.posLims(2));
+        else
+            set(h.eventHands(e), 'XData', [], 'YData', []);
+        end
+    else
+        set(h.eventHands(e), 'XData', [], 'YData', []);
+    end
+end
+
+for t = 1:length(tr)
+    if tr(t).visible         
+        incl = tr(t).t>p.window(1) & tr(t).t<p.window(2);
+        set(h.traceHands(t), 'XData', tr(t).t(incl), ...
+            'YData', tr(t).v(incl)*tr(t).scale*diff(p.posLims)+tr(t).offset*diff(p.posLims));
+    else
+        set(h.traceHands(t), 'XData', [], 'YData', []);
+    end
+end
+
+set(h.yPosDisplay, 'String', sprintf('ordered by %s', sp.yAxOrderings(p.yAxInd).name));
+
+set(h.rasterAx, 'XLim', p.window, 'YLim', p.posLims)
+
+function recolor(ud)
+
+sp = ud.sp;
+p = ud.params;
+h = ud.hands;
+
+set(h.ColorDisplay, 'String', sprintf('colored by %s', sp.colorings(p.colorInd).name));
+
+for c = 1:length(sp.cids)    
+    set(h.rasterHands(c), 'Color', sp.colorings(p.colorInd).colors(c,:));
+end
+
+function rastClick(keydata, f)
+clickX = keydata.IntersectionPoint(1);
+ud = get(f, 'UserData');
+set(ud.hands.userLine, 'XData', clickX*[1 1]);
+
+function selectTrace(f, t)
+ud = get(f, 'UserData');
+p = ud.params;
+tr = ud.traces;
+if p.selectedTrace == t    
+    tr(t).visible = false;
+    ud.hands.trLeg(t).FontWeight = 'normal';   
+    p.selectedTrace = [];
+else
+    if ~isempty(p.selectedTrace)
+        ud.hands.trLeg(p.selectedTrace).FontWeight = 'normal';    
+    end
+    ud.hands.trLeg(t).FontWeight = 'bold';
+    p.selectedTrace = t; 
+    tr(t).visible = true;
+end
+ud.traces = tr;
+ud.params = p;
+set(f, 'UserData', ud);
+updatePlots(ud);
+
+function toggleEvent(f, e)
+ud = get(f, 'UserData');
+p = ud.params;
+ed = ud.eventData;
+if ed(e).visible    
+    ed(e).visible = false;
+    ud.hands.evLeg(e).FontWeight = 'normal';   
+else
+    ed(e).visible = true;
+    ud.hands.evLeg(e).FontWeight = 'bold';   
+end
+ud.eventData = ed;
+ud.params = p;
+set(f, 'UserData', ud);
+updatePlots(ud);
+
+function keyboardPressCallback(f, keydata)
+
+ud = get(f, 'UserData');
+p = ud.params;
+tr = ud.traces;
+switch keydata.Key
+    case 'rightarrow'
+        p.currT = p.currT+p.windowSize/4;
+        if p.currT>ud.sp.st(end); p.currT = 0; end
+        p = updateWindow(p);        
+        if p.movieT<p.window(1)
+            p.movieT = p.window(1);
+        end
+    case 'leftarrow' 
+        p.currT = p.currT-p.windowSize/4;
+        if p.currT<0; p.currT = ud.sp.st(end); end
+        p = updateWindow(p);    
+        if p.movieT>p.window(2)
+            p.movieT = p.window(1);
+        end
+    case 'uparrow'
+        p.windowSize = p.windowSize*0.75;
+        p = updateWindow(p);
+        if p.movieT<p.window(1) || p.movieT>p.window(2)
+            p.movieT = p.window(1);
+        end
+    case 'downarrow'
+        p.windowSize = p.windowSize/0.75;
+        p = updateWindow(p);
+        if p.movieT<p.window(1) || p.movieT>p.window(2)
+            p.movieT = p.window(1);
+        end
+    case 'y'
+        p.yAxInd = p.yAxInd+1;
+        if p.yAxInd>numel(ud.sp.yAxOrderings); p.yAxInd = 1; end
+    case 'c'
+        p.colorInd = p.colorInd+1;
+        if p.colorInd>numel(ud.sp.colorings); p.colorInd = 1; end
+        recolor(ud);
+    case 'hyphen'
+        p.rasterScale = p.rasterScale/1.5;
+    case 'equal'
+        p.rasterScale = p.rasterScale*1.5;
+    case 't'
+        newT = inputdlg('jump to time?');
+        newT = str2double(newT{1});
+        if ~isempty(newT)
+            if newT<0
+                p.currT = 0; 
+            elseif newT>ud.sp.st(end)
+                p.currT = ud.sp.st(end);
+            else
+                p.currT = newT;
+            end
+        end
+        p = updateWindow(p);
+    case 'i'
+        tr(p.selectedTrace).offset = tr(p.selectedTrace).offset + 1/25;
+    case 'k'
+        tr(p.selectedTrace).offset = tr(p.selectedTrace).offset - 1/25;
+    case 'l'
+        tr(p.selectedTrace).scale = tr(p.selectedTrace).scale*5/4;
+    case 'j'
+        tr(p.selectedTrace).scale = tr(p.selectedTrace).scale*4/5;
+    case 'p' 
+        if p.isPlaying
+            p.isPlaying = false;
+        else
+            p.isPlaying = true;
+            p.lastRealTime = now;
+        end
+    case 's'
+        p.movieRate = p.movieRate*4/5;
+    case 'f'
+        p.movieRate = p.movieRate*5/4;
+            
+
+end
+ud.params = p;
+ud.traces = tr;
+set(f, 'UserData', ud)
+updatePlots(ud);
+
+function p = updateWindow(p)
+p.window = p.windowSize*0.5*[-1 1]+p.currT;
+
+function closeFigure(f, myTimer)
+stop(myTimer);
+delete(myTimer);
+delete(f);
