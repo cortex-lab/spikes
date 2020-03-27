57078aa6185b8e1eb5bac00dfad5c4cda6d2433d
Nick Steinmetz <nick.steinmetz@gmail.com>
2020-03-27 12:24:23 -0700

    update eventLockedAvg to take a vector of times for the calculation window

--- analysis/eventLockedAvg.m
+++ analysis/eventLockedAvg.m
@@ -1,6 +1,6 @@
 
 function [avgPeriEventV, winSamps, periEventV, sortedLabels] = eventLockedAvg(V, t, eventTimes, eventLabels, calcWin)
-% function [avgPeriEventV, periEventV] = eventLockedAvgSVD(V, t, eventTimes, eventLabels, calcWin)
+% function [avgPeriEventV, periEventV] = eventLockedAvg(V, t, eventTimes, eventLabels, calcWin)
 %
 % Inputs: 
 % - V: nTraces x nTimePoints
@@ -10,7 +10,12 @@ function [avgPeriEventV, winSamps, periEventV, sortedLabels] = eventLockedAvg(V,
 % value or some text label. If this is a cell array, the "tuning curve"
 % will be plotted evenly spaced; if numeric array then these will be the
 % x-axis values of the tuning curve
-% - window: 1 x 2, the start and end times relative to the event
+% - calcWin: 1 x 2, the start and end times relative to the event
+%   - if calcWin is instead a vector with more than two elements, these are
+%   taken to be the literal times around the event at which you want to
+%   sample. E.g. you can use [-0.5 1.0] for the first version, or
+%   [-0.5:0.1:1.0] for the second version (which allows you to specify the
+%   sampling density)
 %
 % Outputs:
 % - avgPeriEventV: nEventTypes x nTraces x nTimePoints, average temporal
@@ -22,16 +27,21 @@ function [avgPeriEventV, winSamps, periEventV, sortedLabels] = eventLockedAvg(V,
 
 t = t(:)'; % make row
 eventTimes = eventTimes(:)';
-[eventTimes, ii] = sort(eventTimes);
-sortedLabels = eventLabels(ii);
+[eventTimes, ii] = sort(eventTimes); %event times need to be in order for the interp function
+sortedLabels = eventLabels(ii); % but that means the labels are rearranged too, and the user needs to know
 
 nTraces = size(V,1);
 
 eLabels = unique(eventLabels);
 nConditions = length(eLabels);
 
-Fs = 1/median(diff(t));
-winSamps = calcWin(1):1/Fs:calcWin(2);
+if numel(calcWin)==2
+    Fs = 1/median(diff(t));
+    winSamps = calcWin(1):1/Fs:calcWin(2);
+else
+    winSamps = calcWin(:)';
+end
+
 periEventTimes = bsxfun(@plus, eventTimes', winSamps); % rows of absolute time points around each event
 periEventV = zeros(nTraces, length(eventTimes), length(winSamps));
 for s = 1:nTraces
